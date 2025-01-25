01fa58e13c8c990a6a0d8fd9471459adc1dc8e88
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-25 18:35:00 -0500

    Update CartApi.js

--- dummyapi/src/components/CartApi.js
+++ dummyapi/src/components/CartApi.js
@@ -7,8 +7,8 @@ function CartApi(){
     async function getAllCarts(){
         let response=await fetch('https://dummyjson.com/carts');
         let result=await response.json();
-        setCartIds(result.carts);
-        console.log(cartIds);
+        setCartIds(result.carts.map(cart=>cart.id));
+       
 
     }
 
