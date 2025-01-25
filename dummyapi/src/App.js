8a26f4acb2dade61975c9bd858737e9489a6a403
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-25 18:02:18 -0500

    update

--- dummyapi/src/App.js
+++ dummyapi/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import CartApi from './components/CartApi';
 
 function App() {
   return (
     <>
+    <CartApi />
     </>
   );
 }
--- dummyapi/src/components/CartApi.js
+++ dummyapi/src/components/CartApi.js
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react";
+
 function CartApi(){
-    
+
+    let[cartIds,setCartIds]=useState([]);
+
+    async function getAllCarts(){
+        let response=await fetch('https://dummyjson.com/carts');
+        let result=await response.json();
+        setCartIds(result.carts);
+
+    }
+
+    useEffect(()=>{
+        getAllCarts()
+    })
+
     return(
         <>
         
