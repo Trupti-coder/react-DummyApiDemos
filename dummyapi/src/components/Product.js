8b6107d8712fa7275b7d056539e12eed1ab19b20
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-27 10:17:25 -0500

    Update Product.js

--- dummyapi/src/components/Product.js
+++ dummyapi/src/components/Product.js
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 function Product(){
+    let[data,setData]=useState([]);
+    async function getAllProduct(){
+        let response=await fetch("");
+        let result=await response.json();
+        setData(result.products)
+    }
     return(
         <>
         </>
