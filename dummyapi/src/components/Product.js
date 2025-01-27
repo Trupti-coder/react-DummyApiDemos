ad00c28c6b9b2f9068bf860bfe7529336bd7aec2
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-27 11:23:37 -0500

    Update Product.js

--- dummyapi/src/components/Product.js
+++ dummyapi/src/components/Product.js
@@ -14,7 +14,31 @@ function Product(){
 
     return(
         <>
-       
+        <table>
+            <tr>
+                <th>Product Id</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Rating</th>
+                <th>Image</th>
+                <th>Action</th>
+                
+            </tr>
+            {
+                data.map((x)=>{
+                    return(
+                        <tr key={x.id }>
+                            <td>{x.id}</td>
+                            <td>{x.title}</td>
+                            <td>{x.price}</td>
+                            <td>{x.rating}</td>
+                            <td><img src={x.thumbnail} alt=''></img></td>
+
+                        </tr>
+                    )
+                })
+            }
+        </table>
         </>
     );
 }
